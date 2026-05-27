bd855c8c49edd3dfa6d7d680c836c2ee46b5e94e
The-Smarts8184 <lokanshu.ghorai@gmail.com>
2026-05-26 21:01:20 -0500

    with inroduction in the lab

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Lokanshu Ghorai" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print(f" Hi my name is {name}, I am an emerging developer specializing in the intersection of Computer Science, Robotics Engineering, and Systems Architecture. Outside  of building embedded software and hardware design, I am a competitive athlete who runs High School Cross Country and Track, and plays club soccer at the ECNL level.")
 
 if __name__ == "__main__":
     main()
